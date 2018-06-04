bd0fe4b85b5cac208ef4163b9de46e27b3b30d82
杨冠超 <guanchao.ygc@alibaba-inc.com>
2018-06-04 18:24:41 +0800

    Compatible for sofa-commont-utils

---
+++ src/main/java/com/alipay/sofa/common/log/ReportUtil.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.common.log;
+
+/**
+ * ReportUtil 主要是兼容过去很多基础 jar 的用法
+ * <p>
+ * 参照:slf4j 输出信息到控制台及默认（业务app中配置的）日志
+ * <p>
+ * Created by yangguanchao on 18/06/04.
+ */
+public class ReportUtil extends com.alipay.sofa.common.utils.ReportUtil{
+
+}
+
+
