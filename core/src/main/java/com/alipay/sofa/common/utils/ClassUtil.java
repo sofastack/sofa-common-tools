061309bca8a7c6252897f14281364c26d610c248
纶珥 <guaner.zzx@alibaba-inc.com>
2020-03-30 13:03:33 +0800

    move ClassUtil new method location

--- core/src/main/java/com/alipay/sofa/common/utils/ClassUtil.java
+++ core/src/main/java/com/alipay/sofa/common/utils/ClassUtil.java
@@ -33,41 +33,6 @@ import java.util.WeakHashMap;
 public class ClassUtil {
     private static final Map TYPE_MAP = Collections.synchronizedMap(new WeakHashMap());
 
-    /**
-     * Get field from specified object, will lookup in super class until found
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T getField(String fieldName, Object o) {
-        Class<?> klass = o.getClass();
-        while (klass != null) {
-            try {
-                Field f = klass.getDeclaredField(fieldName);
-                f.setAccessible(true);
-                return (T) f.get(o);
-            } catch (Exception e) {
-                klass = klass.getSuperclass();
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Set field of specified object to value, will try to operate on super class until success
-     */
-    public static <T> void setField(String fieldName, Object o, T value) {
-        Class<?> klass = o.getClass();
-        while (klass != null) {
-            try {
-                Field f = klass.getDeclaredField(fieldName);
-                f.setAccessible(true);
-                f.set(o, value);
-                return;
-            } catch (Exception e) {
-                klass = klass.getSuperclass();
-            }
-        }
-    }
-
     public ClassUtil() {
     }
 
@@ -532,6 +497,41 @@ public class ClassUtil {
 
         public List getInterfaces() {
             return Collections.unmodifiableList(this.interfaces);
+        }
+    }
+
+    /**
+     * Get field from specified object, will lookup in super class until found
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getField(String fieldName, Object o) {
+        Class<?> klass = o.getClass();
+        while (klass != null) {
+            try {
+                Field f = klass.getDeclaredField(fieldName);
+                f.setAccessible(true);
+                return (T) f.get(o);
+            } catch (Exception e) {
+                klass = klass.getSuperclass();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Set field of specified object to value, will try to operate on super class until success
+     */
+    public static <T> void setField(String fieldName, Object o, T value) {
+        Class<?> klass = o.getClass();
+        while (klass != null) {
+            try {
+                Field f = klass.getDeclaredField(fieldName);
+                f.setAccessible(true);
+                f.set(o, value);
+                return;
+            } catch (Exception e) {
+                klass = klass.getSuperclass();
+            }
         }
     }
 }
