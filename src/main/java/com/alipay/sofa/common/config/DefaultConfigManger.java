349d3ae22b165e61a7c1c920fbdb43eb13c2e2ec
“HzjNeverStop” <441627022@qq.com>
2021-09-28 19:22:04 +0800

    fix ConfigManager concurrent problem

--- src/main/java/com/alipay/sofa/common/config/DefaultConfigManger.java
+++ src/main/java/com/alipay/sofa/common/config/DefaultConfigManger.java
@@ -20,7 +20,6 @@ import com.alipay.sofa.common.utils.OrderComparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
